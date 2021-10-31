73fa5d1162fb80baba349ed262816770beb5d12f
Arthur Borges <arthurborges.dev@gmail.com>
2021-10-31 20:22:11 -0300

    Add button without functionality inside a conditional rendering ternary and put the image inside div for later resizing

--- src/Product.js
+++ src/Product.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 
-const Product = ({ data }) => (
-  <>
-    <img src={data.image} alt={`the visual representation of a ${data.title}`} />
-    <h3>{data.title}</h3>
-    <p>{data.description}</p>
-    <h4>{data.price}</h4>
-    <p>{data.contact}</p>
-  </>
-);
+const Product = ({ data }) => {
+  const intention = useRef(null);
+  return (
+    <div>
+      <div>
+        <img src={data.image} alt={`the visual representation of a ${data.title}`} />
+      </div>
+      <h3>{data.title}</h3>
+      <p>{data.description}</p>
+      <h4>{data.price}</h4>
+      {
+        intention.current ? (
+          <p>{data.contact}</p>
+        ) : (
+          <button type="button">Show contact</button>
+        )
+      }
+    </div>
+  );
+};
 
 Product.propTypes = {
   data: PropTypes.objectOf(PropTypes.string).isRequired,
