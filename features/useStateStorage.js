cfac1992e00312c066ad49d4445ac875b37479e0
Arthur Borges <arthurborges.dev@gmail.com>
2021-11-01 19:51:44 -0300

    Add useStateStorage Hook developed by Nick Foscarini (got the snippet on his Linkedin post)

---
+++ features/useStateStorage.js
@@ -0,0 +1,26 @@
+import { useEffect, useState, useCallback } from 'react';
+
+export const useStateStorage = (key, defaultValue) => {
+  const [value, setState] = useState(defaultValue);
+
+  useEffect(() => {
+    const store = localStorage.getItem(key);
+    if (store != null) {
+      try {
+        setState(JSON.parse(store));
+      } catch (err) {
+        localStorage.removeItem(key);
+      }
+    }
+  }, [key]);
+
+  const setValue = useCallback(
+    newValue => {
+      setState(newValue);
+      localStorage.setItem(key, JSON.stringify(newValue));
+    },
+    [key]
+  );
+
+  return [value, setValue];
+};
